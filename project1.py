cc967c3790a974a37cb2df070184d68abac50963
Nathan Frohbieter <nathanrfro83@gmail.com>
2020-04-30 14:33:01 -0500

    Added Player Name usage

--- project1.py
+++ project1.py
@@ -1,8 +1,11 @@
 # Generate Random Number
 import random
+
+# Ask for user Name
+player_name = input("Please Enter Your Name: ")
 # Introduce Game
 def start_game():
-    print("This is a game where you pick a number 1 through 10")
+    print(f"Hello, {player_name}. This is a game where you pick a number 1 through 10")
 # Play the game
 def play_game():
     random_number = random.randint(1,10)
@@ -15,7 +18,7 @@ def play_game():
         else:
             if guess_number == random_number:
                 attempts += 1
-                print(f"Congradulations {random_number} was the right number! It took you {attempts} to get it right.")
+                print(f"Congradulations {player_name}! {random_number} was the right number! It took you {attempts} to get it right.")
                 break
             elif guess_number < random_number and guess_number >= 1:
                 attempts += 1
@@ -28,17 +31,17 @@ def play_game():
             else:
                 print(f"Your guess of {guess_number} was outside the range. Remember we are playing from 1 to 10.")
                 continue
-            print("\nYou have finished the game.")
+            print(f"\nGood job {player_name} You have finished the game.")
 
 # Play another Game
 def another_game():
     while True:
-        play_again = input("\nWould you like to play again? (yes or no?)")
+        play_again = input(f"\nWould you like to play again {player_name}? (yes or no?)")
         if play_again.lower() == "yes":
             play_game()
             continue
         elif play_again.lower() == "no":
-            print("Thanks for playing. Hoper to see you soon.")
+            print(f"Thanks for playing {player_name} . Hoper to see you soon.")
             break
         else:
             print("\nInvalid Input. Please Try Again.")
@@ -52,9 +55,9 @@ while True:
         play_game()
         another_game()
         break
-        print("Thanks for playing the game. Hope to see you soon!")
+        print(f"Thanks for playing the game {player_name} . Hope to see you soon!")
     elif want_to_play.lower() == "no":
-        print("Hope to see you again.")
+        print(f"Hope to see you again. {player_name} ")
         break
     else:
         print("Sorry Invalid Input.")
